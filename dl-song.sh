5d0e7e3405bb0efa631c80f679e1103a5891c269
Romain Durade <durade.romain@gmail.com>
2023-12-04 14:02:40 +0100

    inc tracklist downloader

--- dl-song.sh
+++ dl-song.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 while IFS="" read -r p || [ -n "$p" ]
 do
-  wget https://www.free-stock-music.com/music/$p.mp3 \
-    -O $PWD/musics/$p.mp3 \
-    --header="Accept: text/html" \
-    --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" \
-    --referer https://www.free-stock-music.com/$p.html 
+
+  trackfile=$PWD/musics/$p.mp3
+  if [ ! -f $trackfile ]
+  then
+     wget https://www.free-stock-music.com/music/$p.mp3 \
+      -O $trackfile \
+      --header="Accept: text/html" \
+      --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" \
+      --referer https://www.free-stock-music.com/$p.html 
+  fi
 done < tracklist
 
 
